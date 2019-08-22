447c91c580989009a0f0ad422e36f92166ed8091
siyaar28 <softkotosh@gmail.com>
2019-08-22 12:49:47 +0300

    first lesson end

---
+++ Project1/Project1/firstSteps.cpp
@@ -0,0 +1,38 @@
+#include "stuff.h"
+
+int myFunc(int y)
+{
+	return y * y - 5 * y + 4;
+}
+
+void firstSteps()
+{
+	/*
+	int x = 10;
+	int y = 5;
+	x += 5;
+	x *= 8;
+	string s = "sdsds";
+	bool log = (5 > 6) || (7 > 8);
+	cout << myFunc(1) << endl;
+	if (x > 100)
+	{
+		cout << "true branch";
+	}
+	else
+	{
+		cout << "lie bracnh";
+	}
+	for (int i = 1; i <= 1024; i *= 2)
+	{
+		cout << i << " ";
+	}
+	*/
+	// cout << prime(1) << " " << prime(5) << " " << prime(10);
+	for (int i = 0; i < 10; i++)
+	{
+		int k = 10;
+		cout << k;
+	}
+	cout << primeCount(10);
+}
--- Project1/Project1/stuff.h
+++ Project1/Project1/stuff.h
@@ -1,11 +1,11 @@
+#pragma once
 #include <iostream>
 #include "primeNumbers.h"
-#include "stuff.h"
 
 using namespace std;
 
-int main()
-{
-	cout << primeCount(10);
-	return 0;
-}
+// ohhh
+void firstSteps();
+
+//
+int myFunc(int y);
